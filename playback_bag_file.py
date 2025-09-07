091fac13edc44337d23d1599ea18f9dde9a5271f
Freeman <freeman@email.com>
2025-09-07 21:51:53 +0800

    Playback depth and RGB stream using .bag file

--- playback_bag_file.py
+++ playback_bag_file.py
@@ -1,30 +1,44 @@
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+from pyrealsense2 import playback
 
-bag_path = 'outdoors.bag'
-def replay_bag_file(bag_path):
-    
-    pipeline = rs.pipeline()
-    config = rs.config()
-    rs.config.enable_device_from_file(config, bag_path, repeat_playback=True)
-    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+bag_path = 'stairs.bag'
+# bag_path = 'd435i_walk_around.bag'
+# bag_path = 'd435i_walking.bag'
+# bag_path = 'depth_under_water.bag'
+# bag_path = 'outdoors.bag'
 
-    pipeline.start(config)
+pipeline = rs.pipeline()
+config = rs.config()
+rs.config.enable_device_from_file(config, bag_path, repeat_playback=True)
+profile = pipeline.start(config)
 
-    try:
-        while True:
-            frames = pipeline.wait_for_frames()
-            depth_frame = frames.get_depth_frame()
-            if not depth_frame:
-                continue
+# Optional: slow down playback for debugging
+device = profile.get_device()
+playback_device = device.as_playback()
+playback_device.set_real_time(False)
 
-            depth_image = np.asanyarray(depth_frame.get_data())
-            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+try:
+    while True:
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+        if not depth_frame:
+            print("No depth frame received.")
+            continue
 
-            cv2.imshow('Simulated Depth Stream', depth_colormap)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-    finally:
-        pipeline.stop()
-        cv2.destroyAllWindows()
+        depth_image = np.asanyarray(depth_frame.get_data())
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+        color_image = np.asanyarray(color_frame.get_data())
+
+        cv2.imshow('Simulated Depth Stream', depth_colormap)
+        cv2.imshow('Simulated RGB Stream', color_image)
+        
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+finally:
+    pipeline.stop()
+    cv2.destroyAllWindows()
