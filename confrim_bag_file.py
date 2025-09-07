fbf7d21d03ad594e7929940a44d8940cbaff7186
Freeman <freeman@email.com>
2025-09-07 16:52:47 +0800

    Depth frame only

--- confrim_bag_file.py
+++ confrim_bag_file.py
@@ -4,10 +4,6 @@ import cv2
 from pyrealsense2 import playback
 
 bag_path = 'stairs.bag'
-# bag_path = 'd435i_walk_around.bag'
-# bag_path = 'd435i_walking.bag'
-# bag_path = 'depth_under_water.bag'
-# bag_path = 'outdoors.bag'
 
 pipeline = rs.pipeline()
 config = rs.config()
@@ -23,6 +19,7 @@ try:
     while True:
         frames = pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
         if not depth_frame:
             print("No depth frame received.")
             continue
@@ -30,9 +27,8 @@ try:
         depth_image = np.asanyarray(depth_frame.get_data())
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
-
-
         cv2.imshow('Simulated Depth Stream', depth_colormap)
+        
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 finally:
