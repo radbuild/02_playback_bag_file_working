d9178c435efc0281e2d45e7cb6cc4e7b5849017b
Freeman <freeman@email.com>
2025-09-11 23:18:54 +0800

    Gesture zone added

--- main.py
+++ main.py
@@ -13,10 +13,13 @@ pygame.display.set_caption("Motion-Based Circle Game")
 
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
 # Game objects
 circle = Circle(width, height)
 score_manager = ScoreManager()
+# Define a rectangular gesture zone
+zone_rect = pygame.Rect(100, 100, 200, 150)
 
 # try:
 #     motion_detector = RealSenseMotion(width, height)
@@ -25,7 +28,6 @@ score_manager = ScoreManager()
 #     if "No device connected" in str(e):
 #         print("[ERROR] No Intel RealSense device detected. Please check USB connection.")
 #         sys.exit(1)
-
 
 clock = pygame.time.Clock()
 running = True
@@ -58,9 +60,17 @@ try:
         # Simulate motion if mouse hovers over the circle
         motion_pos = mouse_pos if circle.check_overlap(mouse_pos) else None
 
+        pygame.draw.rect(screen, GREEN, zone_rect, 2)
+
+        # Check overlap bet. zone and motion
+        # if motion_pos:
+        if zone_rect.collidepoint(mouse_pos):
+            print("Motion detected in gesture zone!")
+
         if motion_pos:
             score_manager.increment()
             circle.schedule_respawn(0.5)
+            print("Pop balloon")
                     
         circle.try_respawn()
         circle.draw(screen)
